1317e1ae0642bdbadf7602b0d8ba811e8514c203
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2020-03-01 08:36:37 +0530

    basic setup of a project

---
+++ todoappenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ashdeep/ashdeeplearn/projects/python_django/TO_DO_APP_Django/todoappenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
